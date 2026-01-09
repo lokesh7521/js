f1e6ad528ce8d9eb238aae312c3b947284581e0b
lokesh976361 <lk.dansroli@gmail.com>
2026-01-09 16:10:52 +0530

    first.js

---
+++ first.js
@@ -0,0 +1,19 @@
+// name="Tony stark";
+// console.log(name);
+// age=18;
+// console.log(age);
+// price=99.99;
+// console.log(price);
+// x=null;
+// y=undefined;
+// console.log(x);
+// console.log(y);
+/*const profile= {
+    username:"@lokeshlumawat5163",
+    isfollow:false,
+    following:123,
+    followers:123
+};
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile["username"]);*/
