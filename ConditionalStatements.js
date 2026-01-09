deb5405e85762a5e6cefc2a852c3e4d71f8ac54e
lokesh976361 <lk.dansroli@gmail.com>
2026-01-09 16:12:41 +0530

    conditional Statements.js

---
+++ ConditionalStatements.js
@@ -0,0 +1,55 @@
+//Conditional Statements
+
+//if:
+/*let age=25;
+if(age>=18){
+    console.log("you can vote");
+}*/
+
+//else:
+/*let age=25;
+if(age<=18){
+    console.log("you can't vote");
+}
+else{
+    console.log("you can vote")
+}
+*/
+
+//else-if:
+
+/*let age=18;*/
+/*if(age>=25){
+    console.log("yes");
+}
+else if(age<=15){
+    console.log(No);
+}
+else{
+    console.log(null);
+}*/
+
+/*let result=age>=18?"Yes":"No";
+console.log(result);*/
+/*console.log(age>=18?"Yes":"No");*/
+
+/*
+let score = prompt("enter your score(0-100)");
+let grade;
+
+if(score>=90 && score<=100){
+    grade="A";
+}else if (score>=70 && score<=89){
+    grade="B";
+
+}else if (score>=60 && score<=69){
+    grade="C";
+
+}else if (score>=50 && score<=59){
+    grade="D";
+
+}else if (score>=0 && score<=49){
+    grade="F";
+}
+console.log("according to your score, your grade wes:",grade);
+*/
