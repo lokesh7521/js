88e66b7d333d09c94e1d5b183b71df72f115b919
lokesh976361 <lk.dansroli@gmail.com>
2026-01-10 22:37:02 +0530

    Operators.js

---
+++ operators.js
@@ -0,0 +1,45 @@
+//Artimetic Operation
+/*let a=5;
+let b=2;
+console.log("a=",a,"b=",b);
+console.log("a+b=", a+b);
+console.log("a-b=", a-b);
+console.log("a*b=", a*b);
+console.log("a/b=", a/b);
+console.log("a%b=", a%b);*/
+
+//Unary Operators
+/*let a=5;
+let b=2;
+console.log("a=",a, "b=",b);
+a=a-1;
+console.log("a=",a);
+a--;
+console.log(a);
+console.log("a=",a, "b=",b);
+a=a+1;
+console.log("a=",a);
+a++;
+console.log(a);*/
+
+
+//Asignment Operators: +=,-=,=,*=,%=,**=
+
+/*let a=5;
+let b=2;
+a +=4;
+console.log(a);*/
+
+//Comparison Operators:==,!=,===,!==,<,>,<=,>=
+/*let a=5;
+let b=2;
+console.log(a==b);
+console.log(a!=b);*/
+
+
+//Logical operators:AND(&&),OR(||),NOT(!)
+/*let a=5;
+let b=2;
+console.log(a==b&&a>b);
+console.log(a==b||a>b);
+console.log(!(a<b));*/
